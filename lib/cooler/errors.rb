f87f2ff4b27e8da34f2cca48a5228c458a074c17
Ivan Valdes (@ivanvc) <iv@nvald.es>
2012-12-10 16:06:07 -0600

    Add InvalidRecord Error

--- lib/cooler/errors.rb
+++ lib/cooler/errors.rb
@@ -1,5 +1,13 @@
 module Cooler
   class NotFound < NameError
-    def http_status; 404 end
+    def http_status
+      404
+    end
+  end
+
+  class InvalidRecord < StandardError
+    def http_status
+      406
+    end
   end
 end
